549737354f2708ba983c328276f37cace923f429
Duane Moes <moesduane@gmail.com>
2020-11-20 15:37:25 +0100

    last update

--- functions/importing/retrieve_chain.py
+++ functions/importing/retrieve_chain.py
@@ -5,20 +5,17 @@ import warnings
 def retrieve_chain(input_file):
     if input_file.endswith('cif'):
         
-        input_file= 'input_files/mmcif/' + input_file
+        input_file= 'input_files/cif/' + input_file
         #Supress harmless warnings
         with warnings.catch_warnings():
             warnings.simplefilter('ignore', BiopythonWarning)
             #Import the protein data
-            structure = MMCIFParser().get_structure(input_file.replace('.cif','').replace('input_files/mmcif/',''),input_file)
+            structure = MMCIFParser().get_structure(input_file.replace('.cif','').replace('input_files/cif/',''),input_file)
    
     else:
         input_file= 'input_files/pdb/' + input_file
-        #Supress harmless warnings
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', BiopythonWarning)
-            #Import the protein data
-            structure = PDBParser(PERMISSIVE=1).get_structure(input_file.replace('.pdb','').replace('input_files/pdb/',''),input_file)
+       
+        structure = PDBParser(PERMISSIVE=1).get_structure(input_file.replace('.pdb','').replace('input_files/pdb/',''),input_file)
      
     chain = structure[0]['A']
 
